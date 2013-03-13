273364d39d235bf9b7b6d279b153da08049fd9a9
Mequrel <mequrel@gmail.com>
2013-03-13 11:32:47 +0100

    Dzielenie czcionki na linijki

--- textdrawer.py
+++ textdrawer.py
@@ -7,6 +7,9 @@ class TextDrawer:
 			print self.font.letters[character.upper()]
 
 
+def splitLetter(letter):
+	return letter.split('\n')
+
 def appendLetter(text, letter):
 	for i in xrange(len(text)):
 		text[i] += letter[i]
