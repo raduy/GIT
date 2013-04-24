0202b719743f3b72174c3412ccef6394a4bf2e03
Dejw92 <dpawlak1992@gmail.com>
2013-04-24 19:19:39 +0200

    Revert "Sprawdzanie liczby argumentow"
    This reverts commit d849d217ec9ee44c829d38bb03e7c7b05360da43.

--- converter.py
+++ converter.py
@@ -10,11 +10,7 @@ def main():
 	drawer.draw(text)
 
 def getText():
-	if len(sys.argv) != 1:
-        print "Bad arguments"
-        sys.exit(1)
-
-    return sys.argv[0]
+	return sys.argv[0]
 
 if __name__ == '__main__':
 	main()
