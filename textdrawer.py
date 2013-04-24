ff28d691cc5c72a6efc8c775ce7c01418d1910c0
Dejw92 <dpawlak1992@gmail.com>
2013-04-24 19:09:50 +0200

    Obsluga pustego napisu w textdrawerze

--- textdrawer.py
+++ textdrawer.py
@@ -2,16 +2,17 @@ class TextDrawer:
 	def setFont(self, font):
 		self.font = font
 
-	def draw(self, text):
-		firstLetter = self.font.letters[text[0].upper()]
-		drawing = splitLetter(firstLetter)
+	 def draw(self, text):
+        if text:
+            firstLetter = self.font.letters[text[0].upper()]
+            drawing = splitLetter(firstLetter)
 
-		for character in text[1:]:
-			 letter = splitLetter(self.font.letters[character.upper()])
-			 drawing = appendLetter(drawing, letter)
+            for character in text[1:]:
+                 letter = splitLetter(self.font.letters[character.upper()])
+                 drawing = appendLetter(drawing, letter)
 
-		for line in drawing:
-			print line
+            for line in drawing:
+                print line
 
 
 def splitLetter(letter):
